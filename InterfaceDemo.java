1a5ce5fea23637b1c4084ef71c5c453391ce0aff
Shantanu Mane <78042851+RndmCodeGuy20@users.noreply.github.com>
2021-08-10 14:10:31 +0530

    InterfaceDemo code UPR : master

--- InterfaceDemo.java
+++ InterfaceDemo.java
@@ -1,13 +1,26 @@
 public class InterfaceDemo {
-    
+    public static void main(String[] args) {
+        Bank bank = new SBI();
+        System.out.println("Rate of Interest : " + bank.rate());
+    }
 }
 
-interface Inter1 {
-
+interface Bank {
+    float rate();
 };
 
+class SBI implements Bank {
+    public float rate() {
+        return 6.5f;
+    }
+}
+
+class PNB implements Bank {
+    public float rate() {
+        return 9.5f;
+    }
+}
+
 /**
- * $class extends class 
- * $class implements interface
- * $interface extends interface
+ * $class extends class $class implements interface $interface extends interface
  */
