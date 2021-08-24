b300cfaac78cdfe0d20de0ea18a48fbfbbdc9a85
Shantanu Mane <78042851+RndmCodeGuy20@users.noreply.github.com>
2021-08-24 13:53:47 +0530

    Exception Handling UPR : master

---
+++ ExcHandling.java
@@ -0,0 +1,10 @@
+public class ExcHandling {
+    public static void main(String[] args) {
+        try {
+            int data = 100 / 0;
+        } catch (ArithmeticException e) {
+            System.out.println(e);
+        }
+        System.out.println("rest of code...");
+    }
+}
