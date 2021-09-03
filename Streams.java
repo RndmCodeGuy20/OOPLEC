9c962da96d588233f6a70098933f2e3e291b7a17
Shantanu Mane <78042851+RndmCodeGuy20@users.noreply.github.com>
2021-09-03 14:19:16 +0530

    File Handling UPR : master

---
+++ Streams.java
@@ -0,0 +1,21 @@
+import java.io.FileOutputStream;
+
+public class Streams {
+    public static void main(String[] args) {
+        try {
+
+            FileOutputStream f1 = new FileOutputStream("Test_File.txt");
+
+            // f1.write(001);
+            String s = "Hello World! My name is Shantanu!";
+            byte b[] = s.getBytes();
+            f1.write(b);
+            f1.close();
+            System.out.println("Saved Successfully!!!");
+
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+    }
+}
